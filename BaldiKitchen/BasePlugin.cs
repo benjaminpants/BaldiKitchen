1d9fca3e308bf264ff8e8403d1e14c23107811e2
benjaminpants <maltbyfamily@gmail.com>
2023-11-21 00:10:42 -0800

    Initial commit

---
+++ BaldiKitchen/BasePlugin.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using BepInEx;
+using HarmonyLib;
+using MTM101BaldAPI;
+using MTM101BaldAPI.AssetManager;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BaldiKitchen
+{
+    [BepInPlugin("mtm101.rulerp.baldiplus.baldikitchen", "Baldi's Kitchen", "0.0.0.0")]
+    public class BaldiKitchenPlugin : BaseUnityPlugin
+    {
+        static FieldInfo FTaudCorrect = AccessTools.Field(typeof(FieldTripManager), "audCorrect");
+        static FieldInfo FTbaldiAnimator = AccessTools.Field(typeof(FieldTripManager), "baldiAnimator");
+        static FieldInfo FTbaldiMan = AccessTools.Field(typeof(FieldTripManager), "baldiMan");
+        static FieldInfo AMAanimator = AccessTools.Field(typeof(AudioManagerAnimator), "animator");
+
+        public static List<SoundObject> SoundObjects;
+
+        public static CampingTripManager CampingTrip;
+        public static GameObject EmptyTrip;
+        public static KitchenFieldTripManager kitchenManager;
+
+        public static Dictionary<string, SoundObject> baldiDialogue = new Dictionary<string, SoundObject>();
+
+        public static Sprite OvenBG;
+        public static Sprite Knob;
+
+        public static Image CreateImage(Sprite spr, Transform parent, Vector3 position)
+        {
+            Image img = new GameObject().AddComponent<Image>();
+            img.gameObject.layer = LayerMask.NameToLayer("UI");
+            img.transform.SetParent(parent);
+            img.sprite = spr;
+            img.gameObject.transform.localScale = Vector3.one;
+            img.rectTransform.offsetMin = new Vector2(-spr.texture.width / 2f, -spr.texture.width / 2f);
+            img.rectTransform.offsetMax = new Vector2(spr.texture.width / 2f, spr.texture.width / 2f);
+            img.rectTransform.anchorMin = new Vector2(0f, 1f);
+            img.rectTransform.anchorMax = new Vector2(0f, 1f);
+            img.transform.localPosition = new Vector3(-240f, 180f) + (new Vector3(position.x, position.y * -1f));
+            return img;
+        }
+
+        void Awake()
+        {
+            Harmony harmony = new Harmony("mtm101.rulerp.baldiplus.baldikitchen");
+            OvenBG = AssetManager.SpriteFromTexture2D(AssetManager.TextureFromMod(this, "Backgrounds", "OvenBG.png"));
+            Knob = AssetManager.SpriteFromTexture2D(AssetManager.TextureFromMod(this, "Interactables", "Knob.png"));
+            CreateNewBaldiVL("kitchen_intro.wav","Baldi_Vfx_Kitchen_Intro");
+            CreateNewBaldiVL("kitchen_end.wav", "Baldi_Vfx_Kitchen_End");
+            CreateNewBaldiVL("kitchen_today.wav", "Baldi_Vfx_Kitchen_Today");
+            CreateNewBaldiVL("kitchen_today_thanksgiving.wav", "Baldi_Vfx_Kitchen_Today_Thanksgiving");
+            CreateNewBaldiVL("oven_preheat_1.wav", "Baldi_Vfx_Kitchen_Preheat_1");
+            CreateNewBaldiVL("oven_preheat_2.wav", "Baldi_Vfx_Kitchen_Preheat_2");
+            CreateNewBaldiVL("food_turkey.wav", "Baldi_Vfx_Kitchen_Food_Turkey");
+            CreateNewBaldiVL("food_stuffing.wav", "Baldi_Vfx_Kitchen_Food_Stuffing");
+            CreateNewBaldiVL("food_pie.wav", "Baldi_Vfx_Kitchen_Food_Pie");
+            harmony.PatchAll();
+        }
+
+        private void CreateNewBaldiVL(string audioname, string subkey)
+        {
+            baldiDialogue.Add(audioname, ObjectCreatorHandlers.CreateSoundObject(AssetManager.AudioClipFromMod(this, "BaldiAudio", audioname), subkey, SoundType.Voice, Color.green));
+        }
+
+        public static T CreateNewFieldTrip<T>(FieldTripObject tripObject) where T : FieldTripManager
+        {
+            T manager = GameObject.Instantiate(EmptyTrip).AddComponent<T>();
+            // initialize all the variables the manager needs to work properly
+            manager.SetVariable("endlessResultsPre", manager.gameObject.GetComponentInChildren<EndlessTripResults>());
+            manager.SetVariable("tripObject", tripObject);
+            FTbaldiAnimator.SetValue(manager, manager.gameObject.GetComponentInChildren<Animator>()); //there should only be one left after the cleaning
+            manager.SetVariable("scoreText", manager.gameObject.transform.Find("Score").gameObject.GetComponent<TMP_Text>());
+            manager.SetVariable("rankDisplay", manager.gameObject.GetComponentInChildren<TripRankDisplay>());
+            manager.SetVariable("baldi", manager.gameObject.transform.Find("Baldi").GetComponent<RectTransform>());
+            FTbaldiMan.SetValue(manager, manager.gameObject.GetComponentInChildren<AudioManagerAnimator>());
+            AMAanimator.SetValue(FTbaldiMan.GetValue(manager), FTbaldiAnimator.GetValue(manager)); //update the animator property of the baldi
+            FTaudCorrect.SetValue(manager, ((SoundObject[])FTaudCorrect.GetValue(CampingTrip)).Clone());
+            manager.SetVariable("baldiStartHeight", 0f);
+            //finalize
+            DontDestroyOnLoad(manager.gameObject);
+            manager.gameObject.name = String.Format("{0}FieldTrip", EnumExtensions.GetExtendedName<FieldTrips>((int)tripObject.trip));
+            tripObject.tripPre = manager;
+            return manager;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void SetVariable(this object me, string name, object setTo)
+        {
+            AccessTools.Field(me.GetType(),name).SetValue(me, setTo);
+        }
+
+        public static void InitializeAllEvents(this StandardMenuButton smb)
+        {
+            smb.OnPress = new UnityEngine.Events.UnityEvent();
+            smb.OnHighlight = new UnityEngine.Events.UnityEvent();
+            smb.OnRelease = new UnityEngine.Events.UnityEvent();
+        }
+    }
+
+    [HarmonyPatch(typeof(NameManager))]
+    [HarmonyPatch("Awake")]
+    class NamePatch
+    {
+        static void Prefix()
+        {
+            BaldiKitchenPlugin.SoundObjects = Resources.FindObjectsOfTypeAll<SoundObject>().Where(x => x.name != null).ToList();
+            BaldiKitchenPlugin.CampingTrip = Resources.FindObjectsOfTypeAll<CampingTripManager>().First();
+            BaldiKitchenPlugin.CampingTrip.gameObject.SetActive(false);
+            GameObject emptyTrip = GameObject.Instantiate<CampingTripManager>(BaldiKitchenPlugin.CampingTrip).gameObject;
+            BaldiKitchenPlugin.CampingTrip.gameObject.SetActive(true);
+            UnityEngine.Object.DestroyImmediate(emptyTrip.GetComponent<CampingTripManager>());
+            GameObject.DestroyImmediate(emptyTrip.transform.Find("Campfires").gameObject);
+            GameObject.DestroyImmediate(emptyTrip.transform.Find("Buttons3").gameObject);
+            GameObject.DestroyImmediate(emptyTrip.transform.Find("Arithmesticks").gameObject);
+            GameObject.DestroyImmediate(emptyTrip.transform.Find("Buttons4").gameObject);
+            GameObject.DestroyImmediate(emptyTrip.transform.Find("Buttons5").gameObject);
+            for (int _ = 0; _ < 2; _++)
+            {
+                GameObject.DestroyImmediate(emptyTrip.gameObject.GetComponent<AudioManager>());
+                GameObject.DestroyImmediate(emptyTrip.gameObject.GetComponent<AudioSource>());
+            }
+            emptyTrip.name = "Fieldtrip Template";
+            GameObject.DontDestroyOnLoad(emptyTrip.gameObject);
+            BaldiKitchenPlugin.EmptyTrip = emptyTrip;
+
+            // create the kitchen manager
+            BaldiKitchenPlugin.kitchenManager = BaldiKitchenPlugin.CreateNewFieldTrip<KitchenFieldTripManager>(ObjectCreatorHandlers.CreateFieldTripObject(EnumExtensions.ExtendEnum<FieldTrips>("Kitchen"), null, "ERROR", "Unknown"));
+            KitchenFieldTripManager km = BaldiKitchenPlugin.kitchenManager;
+            km.Background = BaldiKitchenPlugin.kitchenManager.gameObject.transform.Find("BG").GetComponent<Image>();
+            km.InitializeRoom<KitchenOven>();
+            AudioManager am = km.gameObject.AddComponent<AudioManager>();
+            am.audioDevice = km.gameObject.AddComponent<AudioSource>();
+            am.audioDevice.outputAudioMixerGroup = Resources.FindObjectsOfTypeAll<UnityEngine.Audio.AudioMixerGroup>().First(x => x.name == "Effects");
+            am.audioDevice.ignoreListenerPause = true;
+            km.myMan = am;
+            GameObject.Destroy(BaldiKitchenPlugin.kitchenManager.Background.gameObject);
+        }
+    }
+}
---
+++ BaldiKitchen/KitchenFieldTripManager.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using static BaldiKitchen.KitchenOven;
+using Image = UnityEngine.UI.Image;
+
+namespace BaldiKitchen
+{
+    public enum KitchenState
+    {
+        Intro = 0,
+        MiniIntro = 1,
+        Active = 2
+    }
+
+    public enum KitchenRoom
+    {
+        None = 0,
+        Oven = 1,
+        Main = 2, //place with cupboards and stuff
+        Bowl = 3,
+        Turkey = 4,
+        Table = 5 //dining room table
+    }
+
+    public abstract class KitchenFieldTripRoom : MonoBehaviour
+    {
+        protected Image background => gameObject.transform.Find("BG").GetComponent<Image>();
+        public virtual KitchenRoom room => KitchenRoom.None;
+        public virtual Sprite roomSprite => null;
+        protected KitchenFieldTripManager manager;
+
+        public abstract void PrepareMiniIntro(ref SoundObject[] objs);
+
+        public virtual void Initialize(KitchenFieldTripManager fm)
+        {
+            manager = fm;
+        }
+
+        public abstract void BeginPlay();
+        public abstract void StopPlay();
+        public abstract void Reset();
+    }
+
+    public class KitchenOven : KitchenFieldTripRoom
+    {
+        public override KitchenRoom room => KitchenRoom.Oven;
+        public override Sprite roomSprite => BaldiKitchenPlugin.OvenBG;
+
+        public Knob[] knobs = new Knob[2];
+
+        public int temperature;
+
+        public SoundObject windSound;
+
+        public TMP_Text TemperatureText; //todo: make the oven explode and you lose the minigame instantly if you crank the temp up to 1000
+
+        public void SetTemperature(int toSet)
+        {
+            temperature = toSet;
+            TemperatureText.text = String.Format("{0}°",temperature.ToString());
+        }
+
+        public void AddTemperature(int toAdd)
+        {
+            SetTemperature(temperature + toAdd);
+        }
+
+        public override void Initialize(KitchenFieldTripManager fm)
+        {
+            base.Initialize(fm);
+            windSound = BaldiKitchenPlugin.SoundObjects.First(x => x.name == "ClockWind");
+            knobs[0] = CreateKnob(new Vector3(155f, 230f), () =>
+            {
+                manager.PlaySound(windSound);
+                AddTemperature(-10);
+            }, 15f);
+            knobs[1] = CreateKnob(new Vector3(318f, 230f), () =>
+            {
+                manager.PlaySound(windSound);
+                AddTemperature(10);
+            }, -15f);
+            TemperatureText = GameObject.Instantiate<TMP_Text>(manager.GetScoreText(), transform);
+            TemperatureText.name = "TempText";
+            TemperatureText.text = "NaN";
+            TemperatureText.color = Color.red;
+            TemperatureText.alignment = TextAlignmentOptions.Midline;
+            TemperatureText.rectTransform.offsetMin = new Vector3(-37f,-102f);
+            TemperatureText.rectTransform.offsetMax = new Vector3(30f, -2f);
+            //TemperatureText.transform.localPosition = new Vector3(-37f, -2f);
+        }
+
+        public Knob CreateKnob(Vector3 pos, UnityAction onPress, float toRotate)
+        {
+            Image img = BaldiKitchenPlugin.CreateImage(BaldiKitchenPlugin.Knob, transform, pos);
+            img.name = "Knob";
+            Knob knob = img.gameObject.AddComponent<Knob>();
+            knob.Initialize(toRotate);
+            knob.tag = "Button";
+            knob.onSpin.AddListener(onPress);
+            return knob;
+        }
+
+        public override void BeginPlay()
+        {
+            for (int i = 0; i < knobs.Length; i++)
+            {
+                knobs[i].SetButtonActive(true);
+            }
+        }
+
+        public override void StopPlay()
+        {
+            for (int i = 0; i < knobs.Length; i++)
+            {
+                knobs[i].SetButtonActive(false);
+            }
+        }
+
+        public override void Reset()
+        {
+            StopPlay();
+            SetTemperature(UnityEngine.Random.Range(10,31) * 10);
+            for (int i = 0; i < knobs.Length; i++)
+            {
+                knobs[i].transform.eulerAngles = Vector3.zero;
+            }
+        }
+
+        public override void PrepareMiniIntro(ref SoundObject[] objs)
+        {
+            objs = new SoundObject[]
+            {
+                BaldiKitchenPlugin.baldiDialogue["oven_preheat_1.wav"],
+                BaldiKitchenPlugin.baldiDialogue["food_turkey.wav"],
+                BaldiKitchenPlugin.baldiDialogue["oven_preheat_2.wav"],
+            };
+        }
+
+        public class Knob : MonoBehaviour
+        {
+            public StandardMenuButton button;
+
+            public UnityEvent onSpin = new UnityEvent();
+
+            private float spinAmount;
+
+            public void Initialize(float spin)
+            {
+                button = gameObject.AddComponent<StandardMenuButton>();
+                button.swapOnHigh = false;
+                button.swapOnHold = false;
+                button.image = gameObject.GetComponent<Image>();
+                button.InitializeAllEvents();
+                spinAmount = spin;
+                onSpin.AddListener(() =>
+                {
+                    transform.Rotate(new Vector3(0f, 0f, spinAmount));
+                });
+                SetButtonActive(false);
+            }
+
+            public void SetButtonActive(bool active)
+            {
+                button.enabled = active;
+                if (active)
+                {
+                    button.OnPress = onSpin;
+                }
+                else
+                {
+                    button.OnPress = new UnityEvent();
+                }
+            }
+        }
+    }
+
+
+    public class KitchenFieldTripManager : FieldTripManager
+    {
+        public AudioManager myMan;
+        public Sprite OvenBG = BaldiKitchenPlugin.OvenBG;
+        public Image Background;
+        private int score = 0;
+        private KitchenState state = KitchenState.Intro;
+        private KitchenRoom room;
+        private Dictionary<KitchenRoom, KitchenFieldTripRoom> rooms = new Dictionary<KitchenRoom, KitchenFieldTripRoom>();
+        private KitchenFieldTripRoom currentRoom => rooms[room];
+
+        public void SwitchToRoom(KitchenRoom toSwitch)
+        {
+            if (rooms.ContainsKey(room))
+            {
+                rooms[room].gameObject.SetActive(false);
+            }
+            room = toSwitch;
+            rooms[room].gameObject.SetActive(true);
+            currentRoom.Reset(); //so the room looks the same every time
+        }
+
+        public TMP_Text GetScoreText()
+        {
+            return this.scoreText;
+        }
+
+        // PLACEHOLDER
+        public void PlaySound(SoundObject file)
+        {
+            myMan.PlaySingle(file);
+        }
+
+        public T InitializeRoom<T>() where T : KitchenFieldTripRoom
+        {
+            GameObject root = new GameObject();
+            root.gameObject.layer = LayerMask.NameToLayer("UI");
+            RectTransform rect = root.AddComponent<RectTransform>();
+            rect.offsetMax = new Vector2(240, 180);
+            rect.offsetMin = new Vector2(-240, -180);
+            root.transform.SetParent(transform);
+            Image bg = GameObject.Instantiate<Image>(Background, root.transform);
+            bg.name = "BG";
+            root.gameObject.SetActive(false);
+            T room = root.gameObject.AddComponent<T>();
+            root.gameObject.name = room.room + "Room";
+            bg.sprite = room.roomSprite;
+            room.Initialize(this);
+            rooms.Add(room.room, room);
+            room.transform.SetAsFirstSibling();
+            root.transform.localScale = Vector3.one; //what the fuck
+            return room;
+        }
+
+        public override void Initialize(BaseGameManager bgm)
+        {
+            base.Initialize(bgm);
+            SwitchToRoom(KitchenRoom.Oven);
+            audIntro = new SoundObject[]
+            {
+                BaldiKitchenPlugin.baldiDialogue["kitchen_intro.wav"],
+                BaldiKitchenPlugin.baldiDialogue["kitchen_today_thanksgiving.wav"],
+                BaldiKitchenPlugin.baldiDialogue["kitchen_end.wav"],
+            };
+            UpdateScore();
+            scoreText.color = Color.black;
+            this.rankDisplay.SetRank(3);
+            base.StartCoroutine(PlayRandomBaldiAnimationsTilTalkingsDone());
+            base.StartCoroutine(base.IntroDelay());
+        }
+
+        public override void IntroFinished()
+        {
+            base.IntroFinished();
+            switch (state)
+            {
+                case KitchenState.Intro:
+                    state = KitchenState.MiniIntro; // time to perform this rooms mini intro! (todo: this will probably be moved elsewhere)
+                    currentRoom.PrepareMiniIntro(ref audIntro);
+                    base.StartCoroutine(PlayRandomBaldiAnimationsTilTalkingsDone());
+                    base.StartCoroutine(base.IntroDelay());
+                    break;
+                case KitchenState.MiniIntro:
+                    state = KitchenState.Active;
+                    LowerBaldi();
+                    currentRoom.BeginPlay();
+                    break;
+            }
+        }
+
+        public IEnumerator PlayRandomBaldiAnimationsTilTalkingsDone()
+        {
+            while (baldiMan.filesQueued == 0) yield return null;
+            while (baldiMan.IsPlaying)
+            {
+                RandomBaldiAnimation();
+                yield return new WaitForSeconds(1.75f);
+            }
+            yield break;
+        }
+
+        private void UpdateScore()
+        {
+            this.scoreText.text = "Score: " + this.score.ToString();
+        }
+    }
+}
